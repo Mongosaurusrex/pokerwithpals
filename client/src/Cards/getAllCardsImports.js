c84a16b971ec0f0ca05821a5345174b7d5c9c70f
Nathan Dygant <nathan.dygant@paradoxinteractive.com>
2021-01-04 16:29:58 +0100

    Creating react app + adding basic flip card and pictures of deck of cards

---
+++ client/src/Cards/getAllCardsImports.js
@@ -0,0 +1,109 @@
+import Back from "./cards/1B.svg";
+import _2C from "./cards/2C.svg";
+import _2D from "./cards/2D.svg";
+import _2H from "./cards/2H.svg";
+import _2S from "./cards/2S.svg";
+import _3C from "./cards/3C.svg";
+import _3D from "./cards/3D.svg";
+import _3H from "./cards/3H.svg";
+import _3S from "./cards/3S.svg";
+import _4C from "./cards/4C.svg";
+import _4D from "./cards/4D.svg";
+import _4H from "./cards/4H.svg";
+import _4S from "./cards/4S.svg";
+import _5C from "./cards/5C.svg";
+import _5D from "./cards/5D.svg";
+import _5H from "./cards/5H.svg";
+import _5S from "./cards/5S.svg";
+import _6C from "./cards/6C.svg";
+import _6D from "./cards/6D.svg";
+import _6H from "./cards/6H.svg";
+import _6S from "./cards/6S.svg";
+import _7C from "./cards/7C.svg";
+import _7D from "./cards/7D.svg";
+import _7H from "./cards/7H.svg";
+import _7S from "./cards/7S.svg";
+import _8C from "./cards/8C.svg";
+import _8D from "./cards/8D.svg";
+import _8H from "./cards/8H.svg";
+import _8S from "./cards/8S.svg";
+import _9C from "./cards/9C.svg";
+import _9D from "./cards/9D.svg";
+import _9H from "./cards/9H.svg";
+import _9S from "./cards/9S.svg";
+import _TC from "./cards/TC.svg";
+import _TD from "./cards/TD.svg";
+import _TH from "./cards/TH.svg";
+import _TS from "./cards/TS.svg";
+import _JC from "./cards/JC.svg";
+import _JD from "./cards/JD.svg";
+import _JH from "./cards/JH.svg";
+import _JS from "./cards/JS.svg";
+import _QC from "./cards/QC.svg";
+import _QD from "./cards/QD.svg";
+import _QH from "./cards/QH.svg";
+import _QS from "./cards/QS.svg";
+import _KC from "./cards/KC.svg";
+import _KD from "./cards/KD.svg";
+import _KH from "./cards/KH.svg";
+import _KS from "./cards/KS.svg";
+import _AC from "./cards/AC.svg";
+import _AD from "./cards/AD.svg";
+import _AH from "./cards/AH.svg";
+import _AS from "./cards/AS.svg";
+
+export default {
+  Back,
+  "2C": _2C,
+  "2D": _2D,
+  "2H": _2H,
+  "2S": _2S,
+  "3C": _3C,
+  "3D": _3D,
+  "3H": _3H,
+  "3S": _3S,
+  "4C": _4C,
+  "4D": _4D,
+  "4H": _4H,
+  "4S": _4S,
+  "5C": _5C,
+  "5D": _5D,
+  "5H": _5H,
+  "5S": _5S,
+  "6C": _6C,
+  "6D": _6D,
+  "6H": _6H,
+  "6S": _6S,
+  "7C": _7C,
+  "7D": _7D,
+  "7H": _7H,
+  "7S": _7S,
+  "8C": _8C,
+  "8D": _8D,
+  "8H": _8H,
+  "8S": _8S,
+  "9C": _9C,
+  "9D": _9D,
+  "9H": _9H,
+  "9S": _9S,
+  TC: _TC,
+  TD: _TD,
+  TH: _TH,
+  TS: _TS,
+  JC: _JC,
+  JD: _JD,
+  JH: _JH,
+  JS: _JS,
+  QC: _QC,
+  QD: _QD,
+  QH: _QH,
+  QS: _QS,
+  KC: _KC,
+  KD: _KD,
+  KH: _KH,
+  KS: _KS,
+  AC: _AC,
+  AD: _AD,
+  AH: _AH,
+  AS: _AS,
+};
